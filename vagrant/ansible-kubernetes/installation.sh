f268fc1d1de7e54bf592ad7838b320a46b5359d1
umair.naushad <umair.naushad@emaratech.ae>
2021-06-17 10:15:55 +0400

    Bash file added

--- vagrant/ansible-kubernetes/installation.sh
+++ vagrant/ansible-kubernetes/installation.sh
@@ -2,3 +2,4 @@ ansible -i hosts all -m ping
 ansible-playbook playbook-common.yaml
 ansible-playbook playbook-master.yaml
 ansible-playbook playbook-worker.yaml
+ansible-playbook playbook-nfs-server.yaml
